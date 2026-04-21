b14d87a827b0d60f6eb8c00b3b2c9ccf80cefec4
Paulo Clara <59574041+pjclara@users.noreply.github.com>
2026-04-21 13:58:00 +0100

    Add user and users icons, implement email verification and welcome pages

---
+++ public/build/assets/index-Bx67iLnx.js
@@ -0,0 +1,1 @@
+import{r as u}from"./app-LR_0F8Yr.js";function o(r){const e=u.useRef({value:r,previous:r});return u.useMemo(()=>(e.current.value!==r&&(e.current.previous=e.current.value,e.current.value=r),e.current.previous),[r])}export{o as u};
